48591fa9eb3ca82d0a7b8b72e9097cc84c1cfe55
ama-ch <amach97@gmail.com>
2013-12-15 13:34:28 +0900

    Change running browsers.

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,11 @@ module.exports = function(config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['Chrome', 'ChromeCanary', 'Firefox', 'Safari', 'PhantomJS'],
+    browsers: [
+      'Chrome',
+      'Firefox',
+      'PhantomJS'
+    ],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,

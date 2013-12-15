8ad4f5913137816355f8823105afa345db8cdc30
ama-ch <amach97@gmail.com>
2013-12-15 16:19:09 +0900

    Add a test.

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -2,4 +2,8 @@ describe('hello.js', function() {
   it('hello', function() {
     expect(hello('hoge')).to.be('Hello, hoge');
   });
+
+  it('hi', function() {
+    expect(hi('hoge')).to.be('Hi, hoge');
+  });
 });

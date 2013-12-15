d308547008bdccefec461ca417388812242207e2
ama-ch <amach97@gmail.com>
2013-12-15 23:33:05 +0900

    Add fizzbuzz function.

---
+++ js/fizzbuzz.js
@@ -0,0 +1,14 @@
+function fizzbuzz(num) {
+  var fizz = num % 3 === 0;
+  var buzz = num % 5 === 0;
+  if (fizz && buzz) {
+  	return 'FizzBuzz';
+  }
+  if (fizz) {
+  	return 'Fizz';
+  }
+  if (buzz) {
+  	return 'Buzz';
+  }
+  return num;
+}
